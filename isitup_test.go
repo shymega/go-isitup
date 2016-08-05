47bc5fa175e317166490db8e70c39e3981146dd1
Dom Rodriguez <shymega@shymega.org.uk>
2016-08-05 20:46:30 +0100

    Replace google.com with example.com
    I forgot that google.com has multiple IP addresses.
    
    Hopefully example.com will be fairly stable, and not fail tests.
    
    This has no known adverse effects.

--- isitup_test.go
+++ isitup_test.go
@@ -5,35 +5,35 @@ import (
 )
 
 func TestGetDomain(t *testing.T) {
-	v := GetDomain("google.com", "go-isitup Unit Tests")
-	if v != "google.com" {
-		t.Error("Expected google.com, got", v)
+	v := GetDomain("example.com", "go-isitup Unit Tests")
+	if v != "example.com" {
+		t.Error("Expected example.com, got", v)
 	}
 }
 
 func TestGetIP(t *testing.T) {
-	v := GetIP("google.com", "go-isitup Unit Tests")
-	if v != "216.58.213.110" {
-		t.Error("Expected 216.58.213.110, got", v)
+	v := GetIP("example.com", "go-isitup Unit Tests")
+	if v != "93.184.216.34" {
+		t.Error("Expected 93.184.216.34, got", v)
 	}
 }
 
 func TestGetPort(t *testing.T) {
-	v := GetPort("google.com", "go-isitup Unit Tests")
+	v := GetPort("example.com", "go-isitup Unit Tests")
 	if v != 80 {
 		t.Error("Expected 80, got", v)
 	}
 }
 
 func TestGetStatusCode(t *testing.T) {
-	v := GetStatusCode("google.com", "go-isitup Unit Tests")
-	if v != 302 {
-		t.Error("Expected HTTP code 302, got", v)
+	v := GetStatusCode("example.com", "go-isitup Unit Tests")
+	if v != 200 {
+		t.Error("Expected HTTP code 200, got", v)
 	}
 }
 
 func TestGetResponseTime(t *testing.T) {
-	//	v := GetResponseTime("google.com", "go-isitup Unit Tests")
+	//	v := GetResponseTime("example.com", "go-isitup Unit Tests")
 	// Empty test for now.
 	// Not sure _how_ to test, response times are variable!
 }
