9121363e130f09f73c100b911ce406367365eda9
Dom Rodriguez <shymega@shymega.org.uk>
2018-08-23 23:06:33 +0100

    tests: lowercase 'T' in user agent header

--- isitup_test.go
+++ isitup_test.go
@@ -1,3 +1,19 @@
+/*
+*  Copyright 2018 Dom Rodriguez (shymega)
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*/
+
 package isitup
 
 import (
@@ -6,35 +22,35 @@ import (
 )
 
 func TestGetDomain(t *testing.T) {
-	v := GetDomain("example.com", "go-isitup Unit Tests")
+	v := GetDomain("example.com", "go-isitup Unit tests")
 	if v != "example.com" {
 		t.Error("Expected example.com, got", v)
 	}
 }
 
 func TestGetIP(t *testing.T) {
-	v := GetIP("example.com", "go-isitup Unit Tests")
+	v := GetIP("example.com", "go-isitup Unit tests")
 	if v != "93.184.216.34" {
 		t.Error("Expected 93.184.216.34, got", v)
 	}
 }
 
 func TestGetPort(t *testing.T) {
-	v := GetPort("example.com", "go-isitup Unit Tests")
+	v := GetPort("example.com", "go-isitup Unit tests")
 	if v != 80 {
 		t.Error("Expected 80, got", v)
 	}
 }
 
 func TestGetStatusCode(t *testing.T) {
-	v := GetStatusCode("example.com", "go-isitup Unit Tests")
+	v := GetStatusCode("example.com", "go-isitup Unit tests")
 	if v != 200 {
 		t.Error("Expected HTTP code 200, got", v)
 	}
 }
 
 func TestGetResponseTime(t *testing.T) {
-	v := GetResponseTime("example.com", "go-isitup Unit Tests")
+	v := GetResponseTime("example.com", "go-isitup Unit tests")
 	typ := reflect.TypeOf(v)
 
 	if typ.Kind() != reflect.Float64 {
